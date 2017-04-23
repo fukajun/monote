487f3e1cbaa6f9aa766c44fd28e816096b0724da
J.Fukaya <fukajun.shark@gmail.com>
2017-04-23 22:19:07 +0900

    Display modify time

--- source/container/ListPage.js
+++ source/container/ListPage.js
@@ -4,6 +4,7 @@
 //
 // Vendor
 import React from 'react';
+import moment from 'moment';
 import { Link } from 'react-router-dom';
 //
 // Lib
@@ -19,13 +20,13 @@ export default class ListPage extends React.Component {
         {
           store.list().map((item)=> {
             let key = seq <= 9 ? `[${seq++}]` : ''
-            console.log(item.line())
             return (
             <li className='item' key={item.id}>
                <Link className='item-link' to={`/edit/${item.id}`}>
                 <div className='item-link-title'>
                   {item.title} {key}
                 </div>
+                <div className='item-link-ctime'>{moment(item.ctime).format("Y/M/D hh:mm")}</div>
                 <br />
                 <div className='item-link-info'>
                   <span className='item-link-body'>{item.line().substring(0, 200)}</span>
