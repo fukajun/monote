3d529da42c3f04c455a406bafd021a985518340d
J.Fukaya <fukajun.shark@gmail.com>
2017-05-05 18:08:09 +0900

    Fix indent

--- source/container/ListPage.js
+++ source/container/ListPage.js
@@ -18,28 +18,29 @@ export default class ListPage extends React.Component {
   render() {
     let seq = 0
     let word = this.props.keyword
-    return (<div className='list'>
-      <Tree currentDir={this.props.currentDir} list={this.props.fulllist} onClick={this.props.onClickDir}/>
-      <ul className='items'>
-        {
-          this.props.list.map((item)=> {
-            let key = seq <= 9 ? `[${seq++}]` : ''
-            return (
-              <li className='item' key={item.id}>
-                 <Link className='item-link' to={`/edit/${item.id}`}>
-                  <div className='item-link-title'>
-                    {item.title()} {key}
-                  </div>
-                  <div className='item-link-ctime'>{moment(item.ctime).format("Y/M/D hh:mm")}</div>
-                  <br />
-                  <div className='item-link-info'>
-                    <span className='item-link-body'>{item.line().substring(0, 200)}</span>
-                  </div>
-                </Link>
-              </li>
-            )
-          })
-        }
+    return (
+      <div className='list'>
+        <Tree currentDir={this.props.currentDir} list={this.props.fulllist} onClick={this.props.onClickDir}/>
+        <ul className='items'>
+          {
+            this.props.list.map((item)=> {
+              let key = seq <= 9 ? `[${seq++}]` : ''
+              return (
+                <li className='item' key={item.id}>
+                   <Link className='item-link' to={`/edit/${item.id}`}>
+                    <div className='item-link-title'>
+                      {item.title()} {key}
+                    </div>
+                    <div className='item-link-ctime'>{moment(item.ctime).format("Y/M/D hh:mm")}</div>
+                    <br />
+                    <div className='item-link-info'>
+                      <span className='item-link-body'>{item.line().substring(0, 200)}</span>
+                    </div>
+                  </Link>
+                </li>
+              )
+            })
+          }
       </ul>
     </div>
     )
