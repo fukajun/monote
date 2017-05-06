c96194767d0e5213624703152f7f45afd0a62d10
J.Fukaya <fukajun.shark@gmail.com>
2017-05-07 01:47:34 +0900

    Title display only basename

---
+++ source/models/DirCollection.js
@@ -0,0 +1,29 @@
+import _u from 'underscore'
+import Path from 'path';
+class Dir {
+  constructor(path) {
+    this.path = path
+    this.items = []
+  }
+
+  level() {
+    return this.path.replace(/[^\/]/g, '').length
+  }
+
+  basename() {
+    return Path.basename(this.path)
+  }
+
+}
+export default class DirCollection {
+  constructor(list) {
+    this.items = list
+    let checkDirs = {}
+    this.items.forEach((item)=> {
+      let dir = checkDirs[item.dirname()] || ( checkDirs[item.dirname()] = new Dir(item.dirname())  )
+      console.log(dir)
+      dir.items.push(item)
+    })
+    this.list = _u.map(checkDirs, (k, v)=> k).sort((a,b)=> a.path > b.path)
+  }
+}
