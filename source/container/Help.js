5bedfd72adb73a4343e5d9cbcd4bc9d6da7d33a5
J.Fukaya <fukajun.shark@gmail.com>
2017-05-12 19:09:02 +0900

    Fix typo on help

--- source/container/Help.js
+++ source/container/Help.js
@@ -9,7 +9,7 @@ export default function Help(props) {
            <h2 className='help-title'>Shortcut key</h2>
            <dl>
              <dt>Ctrl + Shift + n </dt> <dd>: Toggle window</dd>
-             <dt>Command + f </dt>      <dd>: Focust keywod box</dd>
+             <dt>Command + f </dt>      <dd>: Focus keyword box</dd>
              <dt>Command + i </dt>      <dd>: Toggle tree</dd>
              <dt>Command + Enter </dt>  <dd>: Back to List page</dd>
              <dt>Command + n </dt>      <dd>: Create new note</dd>
