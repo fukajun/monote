935b73f83b1879f67763f2d82e31080d9ada0a47
J.Fukaya <fukajun.shark@gmail.com>
2017-06-14 01:59:09 +0900

    Refactoring window css

--- source/container/InputWindow.js
+++ source/container/InputWindow.js
@@ -13,14 +13,14 @@ export default class InputWindow extends React.Component {
   render() {
     const props = this.props;
     return (
-      <div className="help-cover" onClick={props.onClose}>
-        <div className="qr-window" onClick={e => e.stopPropagation()}>
-          <a className="help-close" onClick={props.onClose} ><i className="fa fa-close" /></a>
-          <div className="help-contents" >
-            <h2 className="help-title">Change directory</h2>
+      <div className="window-wrapper" onClick={props.onClose}>
+        <div className="window dirpath" onClick={e => e.stopPropagation()}>
+          <a className="window-close" onClick={props.onClose} ><i className="fa fa-close" /></a>
+          <div className="window-contents" >
+            <h2 className="window-title">Change directory</h2>
             <input defaultValue={props.value} ref={(c) => this.input = c} />
             <ul>
-              <li><a onClick={this.okHandler}>OK</a></li>
+              <li><a className="window-ok-button" onClick={this.okHandler}>Save</a></li>
             </ul>
           </div>
         </div>
--- source/container/QR.js
+++ source/container/QR.js
@@ -4,11 +4,11 @@ import QRCode from 'qrcode.react';
 export default function QR(props) {
   const { value } = props;
   return (
-    <div className="help-cover" onClick={props.onClose}>
-      <div className="qr-window">
-        <a className="help-close" onClick={props.onClose} ><i className="fa fa-close" /></a>
-        <div className="help-contents" >
-          <h2 className="help-title">QR Code</h2>
+    <div className="window-wrapper" onClick={props.onClose}>
+      <div className="window qr">
+        <a className="window-close" onClick={props.onClose} ><i className="fa fa-close" /></a>
+        <div className="window-contents" >
+          <h2 className="window-title">QR Code</h2>
           { value.length === 0 ? 'QRコード化したいテキストを選択してください。' : <QRCode value={value} size={256} className="qrcode" /> }
           <div> {value} </div>
         </div>
